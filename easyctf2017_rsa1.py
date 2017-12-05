4c93ec9e1d6e78551745e49cb10c57333e4df5b2
Kevin Phongagsorn <kphongagsorn123@gmail.com>
2017-12-05 12:40:47 -0500

    Update and rename easyctf2017_rsa.py to easyctf2017_rsa1.py

--- easyctf2017_rsa1.py
+++ easyctf2017_rsa1.py
@@ -19,24 +19,4 @@ print libnum.n2s(pow(c, d, n))
 
 #easyctf{wh3n_y0u_h4ve_p&q_RSA_iz_ez_7829d89f}
 
-rsa2.py
-#!/usr/bin/env python
-# install libnum:
-# apt-get install openssl
-# apt-get install pyopenssl
-# https://github.com/hellman/libnum, python setup.py install
 
-
-import libnum
-
-n = 266965481915457805187702917726550329693157 #find p and q; http://factordb.com/index.php?query=266965481915457805187702917726550329693157
-p = 458070420083487550883
-q = 582804455845022449879
-e = 65537
-c = 78670065603555615007383828728708393504251
-
-n=p*q
-phi=(p-1)*(q-1)
-d = libnum.modular.invmod(e, phi)
-
-print libnum.n2s(pow(c, d, n))
