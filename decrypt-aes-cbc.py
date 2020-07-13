b5702f54d00e2c40d496a4e8a3887f9fbe75a158
Kevin <kphongagsorn123@gmail.com>
2020-07-13 08:39:18 -0700

    Update decrypt-aes-cbc.py

--- decrypt-aes-cbc.py
+++ decrypt-aes-cbc.py
@@ -1,12 +1,13 @@
 from Crypto.Cipher import AES
 import binascii, sys
 
-key = "\x27\x46\x5F\xFD\x4C\xCC\x47\x72\xAB\xFD\xD6\x77\xCD\x98\xA0\xFD\x52\x4F\x3C\x11\x7A\xCC\x86\x25\x7B\xA9\x39\x21\xD7\xA1\x66\xD7"
-iv = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+key = ""
+iv = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" #16 bytes
 
-ts0001 = "b70235c3dbf3a83ef6d520d48e7cf65f60a56cc84bddba3ab15a65f2d31e345c06f7da6b13b27c22309380a901940b8d"
-ts0002 = "10a380a57e0825fa7df7034d0dd96154babf00338103bbcca99854e80d2974bff12e333ba11efe4abc70b17fd6362bff"
-ts0003 = "153fba4c0c5008770a123c72bdfb43e2c7abe3bfdb7e0d0172548da92c4f1538d83015de29e07867a92112cb9db4f3eb"
+# cipher text
+ts0001 = ""
+ts0002 = ""
+ts0003 = ""
 
 def decrypt(ciphertext):
 	obj2 = AES.new(key, AES.MODE_CBC,iv)
